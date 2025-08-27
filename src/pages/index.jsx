0a790031dd3a59dbb25005a48e9e48fba11b2d63
unknown <manafikhiamjad@gmail.com>
2025-08-27 15:58:52 +0300

    removed the dynamic import from  indec

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,5 @@
 import dynamic from 'next/dynamic';
-
-const Navbar = dynamic(() => import("../components/Navbar"), {
-  ssr: false,
-});
-const Hero = dynamic(() => import("../components/Hero"), {
-  ssr: false,
-});
-const About = dynamic(() => import("../components/About"), {
-  ssr: false,
-});
-const Experience = dynamic(() => import("../components/Experience"), {
-  ssr: false,
-});
-const Tech = dynamic(() => import("../components/Tech"), {
-  ssr: false,
-});
-const Works = dynamic(() => import("../components/Works"), {
-  ssr: false,
-});
-const Feedbacks = dynamic(() => import("../components/Feedbacks"), {
-  ssr: false,
-});
-const Contact = dynamic(() => import("../components/Contact"), {
-  ssr: false,
-});
-const StarsCanvas = dynamic(() => import("../components/canvas/Stars"), {
-  ssr: false,
-});
+import { Navbar, Hero, About, Experience, Tech, Works, Contact, StarsCanvas } from "../components"
 
 export default function Home() {
    return (
