7c301ac3c568da707f93827b29c975ea5b7cc8d3
unknown <manafikhiamjad@gmail.com>
2025-11-18 14:17:57 +0300

    added vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <>
       <Toaster/>
       <Component {...pageProps} />
+      <Analytics/>
     </>
   ) 
 }
